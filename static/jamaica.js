117de00612b51defc8dba5a88085c253d4d8379d
evronm <evronm@gmail.com>
2023-09-16 09:40:31 -0700

    some reggae changes, more jamaica basics

--- static/jamaica.js
+++ static/jamaica.js
@@ -5,15 +5,19 @@ function init() {
   u('#account_nav a').handle ('click', navclick)
 }
 
-function reggae(resp) {
-  foo=resp.json()
-}
 
 function navclick(e) {
   fetch(e.target.href)
     .then((res) => res.json())
     .then((json) => console.log(json))
     .catch((err) => console.error("error:", err));
+}
 
-
+function Field (spec) {
+  this.name=spec[0];
+  this.type=spec[1];
+  this.restrictions=spec[2];
+  this.dom=() => {
+    return input({type: this.type, name: this.name})
+  }
 }
